03dfc174f7d190edd39e9fbe4926e44c8664bb16
Rosanna Chiu <rosannachiu@MacBook-Pro-7.local>
2019-01-26 09:43:33 -0800

    add local public files

---
+++ public/js/index.js
@@ -0,0 +1,59 @@
+// index.js
+angular.module('angularApp', [])
+  .controller('indexCtrl', function($scope, $http) {
+
+   $scope.product = '';
+   $scope.num1 = 2;
+   $scope.num2 = 3;
+   // $scope.formData = {
+   // 		num1: $scope.num1,
+   // 		num2 : $scope.num2	
+   // }
+   
+   	$scope.multiply = function () {
+   		console.log("triggered multiply")
+	    $http({
+	        url: '/',
+	        method: "POST",
+	    	headers : { 'Content-Type': 'application/json' },
+	    	data:{
+					"data": {
+						"num1": $scope.num1,
+						"num2": $scope.num2
+					}
+				}
+	    })
+	    .then(function successCallback(successResponse) {
+	            // success
+		        $scope.product = successResponse.data.product
+	    	}, 
+		    function errorCallback(failedResponse) { // optional
+		            console.log(failedResponse);
+		    });
+
+	}
+
+	$scope.grabProduct = function(){
+
+		console.log("triggered grabProduct")
+	    $http({
+	        url: '/product',
+	        method: "POST",
+	    	headers : { 'Content-Type': 'application/json' },
+	    	data:{
+			}
+	    })
+	    .then(function successCallback(successResponse) {
+	            // success
+		        $scope.numberPlayer = successResponse.data.product
+	    	}, 
+		    function errorCallback(failedResponse) { // optional
+		            console.log(failedResponse);
+		    });
+		
+	}
+   
+
+  })
+
+   
