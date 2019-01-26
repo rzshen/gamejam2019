d20d31cac2b95c11ac7caad46c7fde01026a6807
daza <zephronica@gmail.com>
2019-01-26 14:12:32 -0800

    phaser things

--- public/index.js
+++ public/index.js
@@ -1,41 +1,12 @@
 // index.js
 angular.module('angularApp', [])
-  .controller('indexCtrl', function($scope, $http) {
 
-   $scope.product = 123;
-   $scope.num1 = 2;
-   $scope.num2 = 3;
-   // $scope.formData = {
-   // 		num1: $scope.num1,
-   // 		num2 : $scope.num2	
-   // }
-   	console.log($scope)
-   	$scope.multiply = function () {
-   		console.log("triggered multiply")
-	    $http({
-	        url: '/',
-	        method: "POST",
-	    	headers : { 'Content-Type': 'application/json' },
-	    	data:{
-					"data": {
-						"num1": $scope.num1,
-						"num2": $scope.num2
-					}
-				}
-	    })
-	    .then(function successCallback(successResponse) {
-	            // success
-		        $scope.product = successResponse.data.product
-	    	}, 
-		    function errorCallback(failedResponse) { // optional
-		            console.log(failedResponse);
-		    });
+.controller('indexCtrl', function($scope) {
+	
+   // Initialize variables
+   $scope.name1 = '';
+   $scope.name2 = '';
+   $scope.greeting1 = `Hello ${$scope.name1}`;
+   $scope.greeting2 = `Hi ${$scope.name2}`;
 
-
-	   // $http.post('/', { 'num1' : 2 ,'num2' : 3 },
-    //     function (response) { $scope.product = response; },
-    //     function (failure) { console.log("failed :(", failure); });
-	}
-   
-
-  })
+})
